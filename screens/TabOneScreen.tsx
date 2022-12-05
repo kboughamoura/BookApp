d3771cec0a4f56c7c9a9b3e6e448a9f168c96434
khaled boughamoura <boughamourakhaled11@gmail.com>
2022-12-05 16:39:04 +0100

    Implementing Provider to switch btw data sources

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -43,7 +43,6 @@ const query = gql`
 
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-
   const [search, setSearch] = useState('');
 
   //if you want the search to be dynamic
@@ -52,13 +51,43 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   // confirm the search when the user presses the search button
   const [runQuery, { data, loading, error }] = useLazyQuery(query);
   
-  console.log(data);
+  const [provider,setProvider] = useState<"googleBooksSearch" | "openLibrarySearch">("googleBooksSearch");
+
+  
+  const parseBook = (item) => {
+    if (provider === "googleBooksSearch") {
+      return {
+        title: item.volumeInfo.title,
+        image: item.volumeInfo.imageLinks?.thumbnail,
+        authors: item.volumeInfo.authors,
+        isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
+      };
+    } else {
+      return {
+        title: item.title,
+        authors: item.author_name,
+        image: `https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`,
+        isbn: item.isbn?.[0],
+      };
+    }
+  };
+
   return (
     <View style={styles.container}>
 
       <View style={styles.header}>
         <TextInput placeholder='Search ...' style={styles.input}  value={search}  onChangeText={setSearch} />
         <Button title='Search' onPress={() => runQuery({ variables: { q: search } })}/>
+      </View>
+
+      <View style={styles.tabs}>
+        <Text 
+        style={provider==="googleBooksSearch"? {fontWeight:'bold', color:"royalblue"}: {}} 
+        onPress={()=>setProvider("googleBooksSearch")} >Google Books</Text>
+
+        <Text 
+        style={provider==="openLibrarySearch"? {fontWeight:'bold', color:"royalblue"}: {}} 
+        onPress={()=>setProvider("openLibrarySearch")}>Open Library</Text>
 
       </View>
 
@@ -70,15 +99,10 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         </View>
       )}
       <FlatList
-        data={data?.googleBooksSearch?.items || []}
+        data={provider === 'googleBooksSearch' ? data?.googleBooksSearch?.items : data?.openLibrarySearch?.docs || []}
         renderItem={({ item }) => (
         <BookItem
-          book={{
-            title: item.volumeInfo.title,
-            image: item.volumeInfo.imageLinks?.thumbnail,
-            authors: item.volumeInfo.authors,
-            isbn: item.volumeInfo.industryIdentifiers?.[0].identifier,
-      }}
+          book={parseBook(item)}
     />
   )}
   showsVerticalScrollIndicator={false}
@@ -113,8 +137,14 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
   },  
+  tabs: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: 50,
+  },
 });
-function runQuery(arg0: { variables: { q: string; }; }): void {
+/*function runQuery(arg0: { variables: { q: string; }; }): void {
   throw new Error('Function not implemented.');
-}
+}*/
 
