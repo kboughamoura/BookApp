e718e434b75b41f9f61c308f0a35fcdc8331528b
khaled boughamoura <boughamourakhaled11@gmail.com>
2022-12-05 15:48:22 +0100

    List of books displayed

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,74 @@
-import { StyleSheet } from 'react-native';
+import { gql,useQuery } from '@apollo/client';
+import React from 'react';
+import { ActivityIndicator, StyleSheet, FlatList } from 'react-native';
+import BookItem from '../components/BookItem';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+//QUERIES
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
+
+
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const {data,loading,error}= useQuery(query,{variables:{q:"React Nativequery"}});
+  console.log(data);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      {loading && <ActivityIndicator   />}
+      {error && (
+        <View>
+          <Text>Error Fetching data</Text>
+          <Text>{error.message}</Text>
+        </View>
+      )}
+      <FlatList
+        data={data?.googleBooksSearch?.items || []}
+        renderItem={({ item }) => (
+        <BookItem
+          book={{
+            title: item.volumeInfo.title,
+            image: item.volumeInfo.imageLinks?.thumbnail,
+            authors: item.volumeInfo.authors,
+            isbn: item.volumeInfo.industryIdentifiers[0].identifier,
+      }}
+    />
+  )}
+  showsVerticalScrollIndicator={false}
+      />
+      
     </View>
   );
 }
@@ -17,8 +76,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:10,
   },
   title: {
     fontSize: 20,
